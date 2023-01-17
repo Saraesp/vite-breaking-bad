2844c3a90e54f40fecc4885648442e5f5c0c6de6
Saraesp <saraesposito145@gmail.com>
2023-01-17 20:56:50 +0100

    stampato titolo AppHeader e stile

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template lang="">
+    <Header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 mt-4">
+                    <h4>Yu-Gi-Oh Api</h4> 
+                </div>
+            </div>
+        </div>
+    </Header>
+</template>
+
+<style lang="scss">
+    header{
+        height: 100px;
+    }
+</style>
